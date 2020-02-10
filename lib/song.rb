7217404563d193783ea1086ca942036e4ba40380
Jordon Terry <jt5493@hotmail.com>
2020-02-10 00:34:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song
-  @@songs = 0
+  @@count = 0
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@songs += 1
+    @@count += 1
   end
 
   def self.count
-    @@songs
+    @@count
   end
 
 
